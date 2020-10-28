31e9695094e54978e7e8375f2567fe36569413cc
Brian Choi <b.choi562@gmail.com>
2020-10-27 19:03:50 -0700

    updated module 2 project with stop propagation and preventdefault

--- js/index.js
+++ js/index.js
@@ -3,8 +3,9 @@
 
 
 
-//(1&2)mouseover & mouseout green
+//(1&2)mouseover & mouseout green & stopPropagation
 const navbar = document.querySelector('.nav');
+const navbarBlog = document.querySelector('.nav-link:nth-of-type(3)');
 
 navbar.addEventListener('mouseover', (event) => {
     event.target.style.color = 'green';
@@ -14,15 +15,33 @@ navbar.addEventListener('mouseout', (event) => {
     event.target.style.color = 'black';
 })
 
+navbarBlog.addEventListener('mouseover',(event)=>{
+    event.target.style.color = 'red';
+    event.stopPropagation();
+})
+
+navbarBlog.addEventListener('mouseout',(event)=>{
+    event.target.style.color = 'black';
+    event.stopPropagation();
+})
+
+
 // (3)double click message
 
 const bigimg = document.querySelectorAll('img');
-
+const medimg = document.querySelector('.intro img');
 
 bigimg.forEach(img => {
-    img.addEventListener('dblclick', () => {
+    img.addEventListener('dblclick', (event) => {
         alert('You clicked on this photo');
+        
     })
+})
+
+medimg.addEventListener('dblclick', (event) =>{
+    alert('You clicked on th is photo');
+    event.target.style.border = '1px solid black';
+    event.stopPropagation();
 })
 
 
@@ -69,10 +88,51 @@ window.addEventListener('load', (event)=>{
 
 
 
-//(7) Select Event
+//(7) Copy Event
 
-const input = document.querySelector('.footer p');
-
-input.addEventListener('select',(event)=>{
-    console.log(`You selected ${input.textContent}`);
+const paragraphs = document.querySelectorAll('p')
+paragraphs.forEach(item => {
+    item.addEventListener('copy', (event) => {
+    alert('Please do not try to copy me!')
 })
+})
+
+//(8) Drag Event
+
+const text = document.querySelectorAll('img');
+
+text.forEach(item => {
+    item.addEventListener('drag', (event)=>{
+        console.log('Please do not drag me');
+    })
+})
+
+//(9 & 10) Mouse enter and Mouse leave event
+
+const mouseEnt = document.querySelectorAll('p');
+
+mouseEnt.forEach(item =>{
+    item.addEventListener('mouseenter',(event)=>{
+        event.target.style.border = '1px solid black';
+    })
+})
+
+mouseEnt.forEach(item=>{
+    item.addEventListener('mouseleave',(event)=>{
+        event.target.style.border = '0px solid black';
+    })
+})
+
+
+//preventdefault
+const navLinks = document.querySelectorAll('.nav');
+
+navLinks.forEach((link)=> {
+    link.addEventListener("click", function (event) {
+      console.log(
+        `disrupting the default behavior of "${event.target.textContent}" link`
+      );
+      event.preventDefault();
+    });
+  });
+
